18f2727a160912ed65a991ee0f68a50ab8ba9760
Kalpana Karra <kkarra@stanford.edu>
2021-11-11 16:07:29 -0800

    save

--- sgd/src/main/java/org/intermine/bio/dataconversion/SgdConverter.java
+++ sgd/src/main/java/org/intermine/bio/dataconversion/SgdConverter.java
@@ -80,7 +80,7 @@ public class SgdConverter extends BioDBConverter {
 	private static final String H_TAXON_ID = "9606";
 	private Item organism;
 	private Map<String, String> featureMap = new HashMap();
-	private static final boolean TEST_LOCAL = false;
+	private static final boolean TEST_LOCAL = true;
 	private String licence;
 
 
